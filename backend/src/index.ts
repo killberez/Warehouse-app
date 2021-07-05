930ca7182cf99ff6d3c80167f789245353b5a62d
Taras Liush <taras.liush@gmail.com>
2021-07-05 17:30:53 +0300

    express

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello Warehouse")
+const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.get("/", (req: any, res: any) => {
+  res.send("HELLO POSTGRE + NODEJS!!!");
+});
+
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
